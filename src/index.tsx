691a1090d7f652f601f14033056f522210de0e34
Damilare Emmanuel <d.dammylarry@gmail.com>
2019-11-14 20:25:05 +0100

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Shit happens</h1>, document.querySelector("#root"));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+//webpack does all kind of things for use like minifying, cass to css on build and a lot more
+
+//i use this to define operations to compile typescript to javascript
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    //use the ts loader when you see a typescript file
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  watch: true,
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  }
+};
